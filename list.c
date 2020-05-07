ee9a52a74f2e44cc84c0e1b678d52b41a45695ea
santhosh <santhosh.kumar@thoughtworks.com>
2020-05-07 09:43:15 +0530

    modified add_unique  function

--- list.c
+++ list.c
@@ -21,10 +21,12 @@ Node_ptr create_newnode(int value)
 Status add_to_end(List_ptr list, int value)
 {
   Node_ptr newnode = create_newnode(value);
-  if(list->head == NULL )
+  if (list->head == NULL)
   {
     list->head = newnode;
-  } else {
+  }
+  else
+  {
     list->last->next = newnode;
   }
   list->last = newnode;
@@ -38,7 +40,7 @@ Status add_to_start(List_ptr list, int value)
   Node_ptr newnode = create_newnode(value);
   newnode->next = list->head;
   list->head = newnode;
-  if(list->last ==  NULL)
+  if (list->last == NULL)
   {
     list->last = newnode;
   }
@@ -49,11 +51,11 @@ Status add_to_start(List_ptr list, int value)
 
 Status insert_at(List_ptr list, int value, int position)
 {
-  if(position < 0 || position > list->count)
+  if (position < 0 || position > list->count)
   {
     return Failure;
   }
-  if(position == 0)
+  if (position == 0)
   {
     return add_to_start(list, value);
   }
@@ -62,7 +64,8 @@ Status insert_at(List_ptr list, int value, int position)
   Prev_Current_Pair_ptr pair = malloc(sizeof(Prev_Current_Pair));
   pair->current = list->head;
 
-  while(temp <= position) {
+  while (temp <= position)
+  {
     pair->prev = pair->current;
     pair->current = pair->current->next;
     temp++;
@@ -78,9 +81,14 @@ Status insert_at(List_ptr list, int value, int position)
 
 Status add_unique(List_ptr list, int value)
 {
-  if(list->last != NULL && list->last->value == value)
+  Node_ptr current = list->head;
+  while (current != NULL)
   {
-    return Failure;
+    if (current->value == value)
+    {
+      return Failure;
+    }
+    current = current->next;
   }
 
   return add_to_end(list, value);
@@ -88,7 +96,7 @@ Status add_unique(List_ptr list, int value)
 
 void display(List_ptr list)
 {
-  if(list->head == NULL)
+  if (list->head == NULL)
   {
     printf("List is empty\n");
     return;
@@ -96,9 +104,9 @@ void display(List_ptr list)
 
   printf("List : ");
   Node_ptr current = list->head;
-  while(current != NULL)
+  while (current != NULL)
   {
-    printf("%d ",current->value);
+    printf("%d ", current->value);
     current = current->next;
   }
   printf("\n");
@@ -106,57 +114,58 @@ void display(List_ptr list)
 
 Status remove_from_start(List_ptr list)
 {
-  if(list->head == NULL)
+  if (list->head == NULL)
   {
     return Failure;
   }
 
   Node_ptr node = list->head;
   list->head = list->head->next;
-  if(list->count == 1)
+  if (list->count == 1)
   {
     list->last = list->head;
   }
   list->count--;
   free(node);
 
-  return Success; 
+  return Success;
 }
 
 Status remove_from_end(List_ptr list)
 {
-  if(list->last == NULL)
+  if (list->last == NULL)
   {
     return Failure;
   }
-  
-  if(list->count == 1)
+
+  if (list->count == 1)
   {
     return remove_from_start(list);
   }
- 
+
   Node_ptr current = list->head;
-  while(current->next != list->last) {
+  while (current->next != list->last)
+  {
     current = current->next;
   }
-  
+
   list->last = current;
   current = current->next;
   list->last->next = NULL;
   list->count--;
   free(current);
-  
+
   return Success;
 }
 
 Status remove_at(List_ptr list, int position)
 {
-  if(position < 0 || position >= list->count)
+  if (position < 0 || position >= list->count)
   {
     return Failure;
   }
 
-  if(position == 0)
+  if (position == 0)
   {
     return remove_from_start(list);
   }
@@ -165,14 +174,15 @@ Status remove_at(List_ptr list, int position)
   Prev_Current_Pair_ptr pair = malloc(sizeof(Prev_Current_Pair));
   pair->current = list->head;
 
-  while(temp <= position) {
+  while (temp <= position)
+  {
     pair->prev = pair->current;
     pair->current = pair->current->next;
     temp++;
   }
 
   pair->prev->next = pair->current->next;
-  if(pair->prev->next == NULL)
+  if (pair->prev->next == NULL)
   {
     list->last = pair->prev;
   }
@@ -186,10 +196,10 @@ Status remove_at(List_ptr list, int position)
 Status remove_first_occurrence(List_ptr list, int value)
 {
   int position = 0;
-  Node_ptr current = list->head; 
-  while(current != NULL)
+  Node_ptr current = list->head;
+  while (current != NULL)
   {
-    if(current->value == value)
+    if (current->value == value)
     {
       return remove_at(list, position);
     }
@@ -204,13 +214,13 @@ Status remove_all_occurrences(List_ptr list, int value)
 {
   int removed_count = 0;
   int result = Success;
-  while(result != Failure)
+  while (result != Failure)
   {
     result = remove_first_occurrence(list, value);
-    result && removed_count++;
+    result &&removed_count++;
   }
 
-  if(removed_count == 0) 
+  if (removed_count == 0)
   {
     return Failure;
   }
@@ -220,20 +230,20 @@ Status remove_all_occurrences(List_ptr list, int value)
 
 Status clear_list(List_ptr list)
 {
-  if(list->head == NULL)
+  if (list->head == NULL)
   {
     return Failure;
   }
 
   Prev_Current_Pair_ptr pair = malloc(sizeof(Prev_Current_Pair));
   pair->current = list->head;
-  while(pair->current != NULL)
+  while (pair->current != NULL)
   {
     pair->prev = pair->current;
     pair->current = pair->current->next;
     free(pair->prev);
   }
-  
+
   list->head = NULL;
   list->last = NULL;
 
@@ -250,9 +260,9 @@ void check_number_exists(List_ptr list, int value)
 {
   int count = 0;
   Node_ptr current = list->head;
-  while(current != NULL)
+  while (current != NULL)
   {
-    if(current->value == value)
+    if (current->value == value)
     {
       printf("%d is present in the list at position %d\n", value, count);
       return;
